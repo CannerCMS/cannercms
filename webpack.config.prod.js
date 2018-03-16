90ba35ce22c3f17a0a86233eb11bccb1ee74df68
Siou <abz53378@gmail.com>
2018-03-16 11:08:52 +0800

    prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -16,6 +16,34 @@ module.exports = {
           loader: 'babel-loader',
         }],
       },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader", // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // translates Less into CSS
+        }]
+      }
     ],
   },
 }
