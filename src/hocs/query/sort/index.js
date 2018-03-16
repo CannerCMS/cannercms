1a057c7bb5a9ec635f44a8f904ddd3507e60c9c8
Siou <abz53378@gmail.com>
2018-03-16 11:05:39 +0800

    use styled-component instead of scss

--- src/hocs/query/sort/index.js
+++ src/hocs/query/sort/index.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import CSSModules from 'react-css-modules';
 import {Row, Col, Select, Icon} from 'antd';
 const Option = Select.Option;
-import styles from './style/sort.scss';
 import {FormattedMessage} from 'react-intl';
 import defaultMessage from '../locale';
+import styled from 'styled-components';
 
-@CSSModules(styles, {allowMultiple: true})
+
+const SortCol = styled(Col)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+const Label = styled.span`
+  font-size: 14px;
+  letter-spacing: 2px;
+  font-weight: 400;
+  color: #aaa;
+`;
+
+const Selector = styled(Select)`
+  width: 150px;
+  margin: 0 15px;
+`;
+
+const OrderSwitch = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+`;
+
+const UpIcon = styled(Icon)`
+  color: ${props => props.order > 0 ? '#333' : '#ccc'};
+`;
+
+const DownIcon = styled(Icon)`
+  color: ${props => props.order > 0 ? '#ccc' : '#333'};
+`
+
 export default class Sort extends Component {
   static propTypes = {
     options: PropTypes.array,
@@ -47,21 +79,21 @@ export default class Sort extends Component {
     const {options} = this.props;
     const {order} = this.state;
     return <Row type="flex" justify="end">
-      <Col styleName="sort">
-        <span styleName="label">
+      <SortCol>
+        <Label>
           <FormattedMessage
             id="query.sort.label"
             defaultMessage={defaultMessage.en['query.sort.label']}
           />
-        </span>
-        <Select styleName="select" onSelect={this.onSelect}>
+        </Label>
+        <Selector onSelect={this.onSelect}>
           {options.map((cond, i) => <Option key={i} value={cond.key}>{cond.title}</Option>)}
-        </Select>
-        <div onClick={this.changeOrder} styleName="order-switch">
-          <Icon styleName={order > 0 ? 'active icon' : 'icon'} type="caret-up" />
-          <Icon styleName={order < 0 ? 'active icon' : 'icon'} type="caret-down" />
-        </div>
-      </Col>
+        </Selector>
+        <OrderSwitch onClick={this.changeOrder}>
+          <UpIcon order={order} type="caret-up" />
+          <DownIcon order={order} type="caret-down" />
+        </OrderSwitch>
+      </SortCol>
     </Row>;
   }
 }
