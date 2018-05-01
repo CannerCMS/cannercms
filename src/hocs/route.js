0be9521d8e3ed29248828f3f65167a57275cb22c
Siou <abz53378@gmail.com>
2018-05-01 19:28:17 +0800

    route hoc

---
+++ src/hocs/route.js
@@ -0,0 +1,87 @@
+// @flow
+import * as React from 'react';
+import type RefId from 'canner-ref-id';
+
+const RENDER_CHILDREN = 0;
+const RENDER_COMPONENT = 1;
+const RENDER_NULL = 2;
+
+type Props = {
+  pattern: string,
+  routes: Array<string>,
+  path: string,
+  params: {
+    op: string
+  },
+  refId: RefId,
+  renderChildren: (Object) => React.Node
+}
+
+export default function withRoute(Com: React.ComponentType<*>) {
+  return class ComWithRoute extends React.Component<Props> {
+    render() {
+      const {routes, pattern,  path, params, refId, renderChildren} = this.props;
+      const renderType = getRenderType({
+        pattern,
+        routes,
+        path,
+        params
+      });
+      return <React.Fragment>
+        {
+          renderType === RENDER_CHILDREN && renderChildren({
+            refId
+          })
+        }
+        {
+          renderType === RENDER_COMPONENT && <Com {...this.props} />
+        }
+      </React.Fragment>
+    }
+  }
+}
+
+function getRenderType({
+  routes,
+  path,
+  params,
+  pattern
+}) {
+  const {op} = params;
+  const paths = genPaths(path, pattern);
+  const pathsLength = paths.length;
+  const routesLength = routes.length;
+  if (routes[0] !== paths[0]) {
+    return RENDER_NULL;
+  }
+  const entryType = pattern.split('.')[0];
+  if (entryType === 'object') {
+    if (routesLength >= pathsLength && !isCompleteContain(paths, routes)) {
+      return RENDER_NULL;
+    }
+    return RENDER_COMPONENT;
+  } else {
+    // array
+    if ((routesLength === 1 && op === 'create') || routesLength === 2) {
+      return RENDER_CHILDREN;
+    } else if (routesLength >= pathsLength && !isCompleteContain(paths, routes)) {
+      return RENDER_NULL;
+    }
+    return RENDER_COMPONENT;
+  }
+}
+
+export function isCompleteContain(paths: Array<string>, routes: Array<string>) {
+  return paths.map((key, i) => routes[i] === key || key === '__ARRAY_INDEX__')
+    .reduce((result, curr) => result && curr);
+}
+
+export function genPaths(path: string, pattern: string) {
+  const patterns = pattern.split('.');
+  const indexs = patterns.map((type, i) => type === 'array' ? i : -1)
+    .filter(index => index !== -1);
+  let paths = path.split('/')
+    .map((route, i) => indexs.indexOf(i) === -1 ? route : [].concat(route, '__ARRAY_INDEX__'))
+    .reduce((result, curr) => result.concat(curr), []);
+  return paths;
+}
---
+++ test/hocs/route.test.js
@@ -0,0 +1,139 @@
+import * as React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
+import RefId from 'canner-ref-id';
+import withHOC, {isCompleteContain, genPaths} from '../../src/hocs/route';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('genPaths', () => {
+  it('should works', () => {
+    const pattern = 'array.object.array.string';
+    const path = 'info/info/info';
+    expect(genPaths(path, pattern)).toEqual(['info', '__ARRAY_INDEX__', 'info', 'info', '__ARRAY_INDEX__']);
+  })
+});
+
+describe('isCompelete', () => {
+  it('should fail', () => {
+    const paths = ['info', 'name'];
+    const routes = ['info', 'hi'];
+    expect(isCompleteContain(paths, routes)).toBeFalsy();
+  });
+
+  it('should true', () => {
+    const paths = ['info', '__ARRAY_INDEX__'];
+    const routes = ['info', 'theisofinfo'];
+    expect(isCompleteContain(paths, routes)).toBeTruthy();
+  });
+});
+
+describe('withRoute', () => {
+  let WrapperComponent, props, MockComponent, mockRenderChildren;
+
+  beforeEach(() => {
+    MockComponent = function MockComponent() {
+      return (<div>Component</div>);
+    }
+    mockRenderChildren = jest.fn().mockImplementation(() => <div>childre</div>);
+    props = {
+      refId: new RefId('posts'),
+      renderChildren: mockRenderChildren
+    }
+    WrapperComponent = withHOC(MockComponent);
+  });
+
+  it('should not render', () => {
+    const routes = ['info'];
+    const path = 'posts';
+    const wrapper = shallow(<WrapperComponent
+      {...props}
+      routes={routes}
+      path={path}
+      pattern="object"
+      params={{}}
+    />);
+    expect(wrapper.find(MockComponent).length).toBe(0);
+  });
+
+  it('should not render', () => {
+    const routes = ['info', 'name'];
+    const path = 'info/nickname';
+    const wrapper = shallow(<WrapperComponent
+      {...props}
+      routes={routes}
+      path={path}
+      pattern="object.string"
+      params={{}}
+    />);
+    expect(wrapper.find(MockComponent).length).toBe(0);
+  });
+
+  it('should not render', () => {
+    const routes = ['posts', 'id1', 'name'];
+    const path = 'posts/nickname';
+    const wrapper = shallow(<WrapperComponent
+      {...props}
+      routes={routes}
+      path={path}
+      pattern="array.string"
+      params={{}}
+    />);
+    expect(wrapper.find(MockComponent).length).toBe(0);
+  });
+
+  it('should render', () => {
+    const routes = ['info'];
+    const path = 'info';
+    const wrapper = shallow(<WrapperComponent
+      {...props}
+      routes={routes}
+      path={path}
+      pattern="object"
+      params={{}}
+    />);
+    expect(wrapper.find(MockComponent).length).toBe(1);
+  });
+
+  it('should render', () => {
+    const routes = ['posts'];
+    const path = 'posts';
+    const wrapper = shallow(<WrapperComponent
+      {...props}
+      routes={routes}
+      path={path}
+      pattern="array"
+      params={{}}
+    />);
+    expect(wrapper.find(MockComponent).length).toBe(1);
+  });
+
+  it('should render children', () => {
+    const routes = ['posts', 'id1'];
+    const path = 'posts';
+    const wrapper = shallow(<WrapperComponent
+      {...props}
+      routes={routes}
+      path={path}
+      pattern="array"
+      params={{}}
+    />);
+    expect(wrapper.find(MockComponent).length).toBe(0);
+    expect(mockRenderChildren).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render children', () => {
+    const routes = ['posts'];
+    const path = 'posts';
+    const wrapper = shallow(<WrapperComponent
+      {...props}
+      routes={routes}
+      path={path}
+      pattern="array"
+      params={{op: 'create'}}
+    />);
+    expect(wrapper.find(MockComponent).length).toBe(0);
+    expect(mockRenderChildren).toHaveBeenCalledTimes(1);
+  });
+});
