18cee74147fcc7c7614b10dd03d138a168eb132e
Siou <abz53378@gmail.com>
2018-05-08 17:24:19 +0800

    temp fix conatinerRouter

--- src/hocs/containerRouter.js
+++ src/hocs/containerRouter.js
@@ -16,7 +16,7 @@ type Props = {
 export default function withContainerRouter(Com: React.ComponentType<*>) {
   return class ContainerWithRouter extends React.Component<Props> {
     render() {
-      const {routes, childrenName, renderChildren, refId} = this.props;
+      const {routes, renderChildren, refId} = this.props;
       return <Context.Provider
         value={{
           renderChildren,
@@ -25,22 +25,6 @@ export default function withContainerRouter(Com: React.ComponentType<*>) {
         }}
       >
         <Com {...this.props}/>
-        {
-          // no need to filter any container so just render
-          (routes.length === 0) && <Com {...this.props}/>
-        }
-        {
-          // the route points on this container so  render it!
-          (routes.length === 1 && childrenName.length === 1 && routes[0] === childrenName[0]) && <Com {...this.props}/>
-        }
-        {
-          // the route points on the children of this container, so render children!
-          (routes.length > 1 && childrenName.length && routes[0] === childrenName[0]) && <div>{renderChildren({refId, routes})}</div>
-        }
-        {
-          // in container's children
-          Boolean(routes.length && childrenName.length && (routes[0] !== childrenName[0]) && (childrenName.indexOf(routes[0]) !== -1)) && <Com {...this.props}/>
-        }
       </Context.Provider>;
     }
   };
