eb5d1aef4c7f8b464353ff3929d09c0a22b9de29
Siou <abz53378@gmail.com>
2018-06-01 20:37:32 +0800

    edit test

--- test/action/actionsToVariables.test.js
+++ test/action/actionsToVariables.test.js
@@ -1,41 +1,113 @@
 import actionsToVariables from '../../src/action/actionsToVariables';
 import {fromJS} from 'immutable';
 
-describe('actions to variables', () => {
-  it('should works', () => {
-    const updateAction = {
-      type: 'UPDATE_ARRAY',
-      payload: {
-        id: 'post1',
-        value: fromJS({
-          "title": "123"
-        })
-      }
-    }
-    const createAndConnectAction = {
-      type: 'CREATE_AND_CONNECT',
-      payload: {
-        id: 'post1',
-        path: 'author',
-        value: fromJS({
-          name: 'newAuthor'
-        })
-      }
-    }
+const updateArrayAction = {
+  type: 'UPDATE_ARRAY',
+  payload: {
+    id: 'post1',
+    value: fromJS({
+      "title": "123"
+    })
+  }
+};
 
-    const disconnectAction = {
-      type: 'DISCONNECT',
-      payload: {
-        id: 'post1',
-        path: 'author',
-        value: fromJS({
-          id: '67428a37-107a-4a59-811d-15810c7c49a9'
-        })
+const updateArrayActionWithNestedArrayAction = {
+  type: 'UPDATE_Array',
+  payload: {
+    id: 'post1',
+    value: fromJS({
+      name: '321',
+      tags: ['1', '2'],
+      info: {
+        images: [{
+          url: 'url1'
+        }]
       }
-    }
+    })
+  }
+}
 
+const createArrayAction = {
+  type: 'CREATE_ARRAY',
+  payload: {
+    id: 'post2',
+    value: fromJS({
+      "title": "123"
+    })
+  }
+};
+
+const deleteArrayAction = {
+  type: 'DELETE_ARRAY',
+  payload: {
+    id: 'post3',
+    value: fromJS({})
+  }
+}
+
+const updateObjectAction = {
+  type: 'UPDATE_OBJECT',
+  payload: {
+    key: 'info',
+    value: fromJS({
+      name: '321'
+    })
+  }
+}
+
+const updateObjectActionWithNestedArrayAction = {
+  type: 'UPDATE_OBJECT',
+  payload: {
+    key: 'info',
+    value: fromJS({
+      name: '321',
+      tags: ['1', '2'],
+      info: {
+        images: [{
+          url: 'url1'
+        }]
+      }
+    })
+  }
+}
+
+const createAndConnectAction = {
+  type: 'CREATE_AND_CONNECT',
+  payload: {
+    id: 'post1',
+    path: 'author',
+    value: fromJS({
+      name: 'newAuthor'
+    })
+  }
+}
+
+const disconnectAction = {
+  type: 'DISCONNECT',
+  payload: {
+    id: 'post1',
+    path: 'author',
+    value: fromJS({
+      id: '67428a37-107a-4a59-811d-15810c7c49a9'
+    })
+  }
+}
+
+describe('single action to variable', () => {
+  test('update array', () => {
+    expect(actionsToVariables([updateArrayAction]))
+      .toMatchObject({
+        payload: updateArrayAction.payload.value.toJS(),
+        where: {id: updateArrayAction.payload.id}
+      });
+  });
+
+});
+
+describe('multiple actions to variables', () => {
+  test('should works', () => {
     expect(actionsToVariables([
-      updateAction,
+      updateArrayAction,
       createAndConnectAction,
       disconnectAction
     ])).toMatchObject({
@@ -56,3 +128,4 @@ describe('actions to variables', () => {
     })
   });
 });
+
