c17c1ee5529db4f1d8ed881865bf4ae5396db42f
chilijung <chilijung@gmail.com>
2018-07-26 16:04:31 +0800

    add comment

--- examples/simple-blog/canner.schema.js
+++ examples/simple-blog/canner.schema.js
@@ -2,6 +2,8 @@
 /* eslint-disable react/prop-types */
 
 import builder from 'canner-script';
+
+// all schema files should end with `*.schema.js`
 import Posts from './schema/posts.schema.js';
 import Authors from './schema/authors.schema.js';
 import Categories from './schema/categories.schema.js';
