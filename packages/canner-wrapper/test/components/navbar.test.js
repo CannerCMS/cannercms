4fd90dbe1362a1ed8d771a56a5e544cc2be58b12
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-03 21:37:34 +0800

    Add tests

---
+++ packages/canner-wrapper/test/components/navbar.test.js
@@ -0,0 +1,71 @@
+
+import * as React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+import {Layout, Menu, Badge} from 'antd';
+
+import Navbar from  '../../src/components/Navbar';
+const {Header} = Layout;
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+const navbarConfig = {
+  logo: 'http://www.img.jpg',
+  showSaveButton: true,
+  renderMenu: jest.fn()
+};
+
+
+const renderComponent = (props) => {
+  return shallow(
+    <Navbar
+      dataChanged={{}}
+      deploy={jest.fn()}
+      {...navbarConfig}
+      {...props}
+    />
+  )
+};
+
+
+describe('<Navbar>', () => {
+  it('should render a Header', () => {
+    const wrapper = renderComponent();
+    expect(wrapper.find(Header).length).toBe(1);
+  });
+
+  it('should render a Menu', () => {
+    const wrapper = renderComponent();
+    expect(wrapper.find(Menu).length).toBe(1);
+  });
+
+  it('should render a Menu.Item', () => {
+    const wrapper = renderComponent();
+    expect(wrapper.find(Menu.Item).length).toBe(1);
+  });
+
+  it('should render a Badge', () => {
+    const wrapper = renderComponent({ dataChanged: { 'key': true } });
+    expect(wrapper.find(Badge).length).toBe(1);
+  });
+
+  // it('should render one Menu.SubMenu', () => {
+  //   const wrapper = renderComponent();
+  //   expect(wrapper.find(Menu.SubMenu).length).toBe(1);
+  // });
+
+  // it('should render null', () => {
+  //   const wrapper = renderComponent({ menuConfig: false });
+  //   expect(wrapper.isEmptyRender()).toBe(true);
+  // });
+
+  it('should call deploy', () => {
+    const deploy = jest.fn()
+      .mockReturnValueOnce(Promise.resolve());
+    const wrapper = renderComponent({ deploy });
+    const menuItem = { key: 'deploy', params: {}};
+    wrapper.find(Menu).simulate('click', menuItem);
+    expect(deploy).toBeCalled();
+  });
+});
