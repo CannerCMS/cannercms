8264c1be630be091a05f24b798ed52c0e57fc76e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-08 16:03:29 +0800

    Implement canner-router

---
+++ packages/router/src/index.js
@@ -0,0 +1,13 @@
+// @flow
+
+import createHistory from 'history/createBrowserHistory';
+import historyRouter from '@canner/history-router';
+
+export default class Router extends historyRouter {
+  constructor({baseUrl = '/'}: {baseUrl: string}) {
+    const history = createHistory({
+      basename: baseUrl,
+    })
+    super({baseUrl, history})
+  }
+}
