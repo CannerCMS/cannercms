7841e63cdf9d0f621174de482b0c9ae5c77d65f9
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-08 14:17:12 +0800

    Update router

--- packages/history-router/src/index.js
+++ packages/history-router/src/index.js
@@ -10,6 +10,15 @@ type HistoryType = {
   push: string => void
 }
 
+type GoToParamsType = {
+  pathname: string,
+  operator?: 'create' | 'update',
+  payload?: Object,
+  where?: Object,
+  sort?: Object,
+  pagination?: Object
+};
+
 export default class HistoryRouter {
   constructor({baseUrl = '/', history}: {baseUrl: string, history: HistoryType}) {
     this.baseUrl = baseUrl;
@@ -22,24 +31,36 @@ export default class HistoryRouter {
     return deleteFirstSlash(pathnameWithoutBaseUrl).split('/');
   }
 
-  getParams = () => {
+  getOperator = () => {
     const {location: {search}} = this.history;
     const qs = queryString.parse(search);
-    if (qs && qs.payload) {
-      return {
-        ...qs,
-        payload: JSON.parse(qs.payload)
-      }
-    }
-    return qs;
+    return qs.operator || 'update';
   }
 
-  goTo = ({pathname, params = {}}: { pathname: string, params: Object}) => {
+  getPayload = () => {
+    return getUrlObjectParams(this.history, 'payload');
+  }
+
+  getWhere = () => {
+    return getUrlObjectParams(this.history, 'where');
+  }
+
+  getSort = () => {
+    return getUrlObjectParams(this.history, 'sort');
+  }
+
+  getPagination = () => {
+    return getUrlObjectParams(this.history, 'pagination');
+  }
+
+  goTo = ({pathname, operator, payload, where, sort, pagination}: GoToParamsType) => {
     const pathnameWithoutFirstSlash = deleteFirstSlash(pathname);
-    const { payload, ...reset } = params;
     const qs = queryString.stringify({
-      ...reset,
+      operator,
       payload: JSON.stringify(payload),
+      where: JSON.stringify(where),
+      sort: JSON.stringify(sort),
+      pagination: JSON.stringify(pagination),
     });
     this.history.push(`${this.baseUrl}/${pathnameWithoutFirstSlash}${qs ? `?${qs}` : ''}`);
   }
@@ -50,4 +71,10 @@ function deleteFirstSlash(path) {
     return path.substring(1);
   }
   return path;
+}
+
+function getUrlObjectParams(history, key) {
+  const {location: {search}} = history;
+  const qs = queryString.parse(search);
+  return qs[key] ? JSON.parse(qs[key]) : {};
 }
