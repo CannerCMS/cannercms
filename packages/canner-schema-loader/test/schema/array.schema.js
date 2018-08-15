d82ca5b332506507c25a390b305272f6cbcde8e1
Siou <abz53378@gmail.com>
2018-08-15 17:57:32 +0800

    add canner-schema-loader

---
+++ packages/canner-schema-loader/test/schema/array.schema.js
@@ -0,0 +1,32 @@
+/** @jsx builder */
+
+import builder from 'canner-script';
+const Arrays = ({attributes}) => <object {...attributes}>
+  <array keyName="tabs" title="Tabs" ui="tab">
+    <string keyName="info" title="info" />
+  </array>
+  <array keyName="panel" ui="panel" title="Panel">
+    <string keyName="info" title="info" />
+  </array>
+  <array keyName="tableroute" ui="tableRoute" title="Table-route" uiParams={{
+    columns: [{
+      title: 'info',
+      dataIndex: 'info'
+    }]
+  }}>
+    <string keyName="info" title="info" />
+  </array>
+  <array keyName="table" ui="table" title="Table" uiParams={{
+    columns: [{
+      title: 'info',
+      dataIndex: 'info'
+    }]
+  }}>
+    <string keyName="info" title="info" />
+  </array>
+  {/* <array keyName="slider" ui="slider" title="Slider" />
+  <array keyName="tag" ui="tag" title="Tag" /> */}
+{/*<array keyName="gallery" ui="gallery" title="Gallery" /> */}
+</object>
+
+export default Arrays
