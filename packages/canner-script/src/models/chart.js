4cb309cbff73332b44dcf34e86499218d1ec56db
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-19 11:27:15 +0800

    Set chart default random key name

--- packages/canner-script/src/models/chart.js
+++ packages/canner-script/src/models/chart.js
@@ -1,11 +1,15 @@
 // @flow
 
 import BasicModel from './basic';
+import {getRandomKey} from '../layout';
 import type {CannerSchema} from '../flow-types';
 
 export default class ChartModel extends BasicModel {
-  constructor(attrs: CannerSchema) {
-    super('chart', attrs);
+  constructor({keyName = getRandomKey() , ...attrs}: CannerSchema) {
+    super('chart', {
+      keyName,
+      ...attrs
+    });
   }
 
   getDefaultUI() {
