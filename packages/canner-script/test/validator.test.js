5097490ae017a44dd609c9ca31c0e00f159a954d
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-19 11:26:14 +0800

    Update tests for indicator tag

--- packages/canner-script/test/validator.test.js
+++ packages/canner-script/test/validator.test.js
@@ -7,6 +7,7 @@ describe('packageName', () => {
     expect(validator.validatePackageName('antd-boolean')).toBe(true);
     expect(validator.validatePackageName('antd-array')).toBe(true);
     expect(validator.validatePackageName('antd-object')).toBe(true);
+    expect(validator.validatePackageName('antd-indicator')).toBe(true);
     expect(validator.validatePackageName('path/to/antd-object')).toBe(true);
     expect(validator.validatePackageName('vega-chart')).toBe(true);
   });
@@ -29,6 +30,7 @@ describe('cannerDataType', () => {
     expect(validator.validateCannerDataType('file')).toBe(true);
     expect(validator.validateCannerDataType('image')).toBe(true);
     expect(validator.validateCannerDataType('chart')).toBe(true);
+    expect(validator.validateCannerDataType('indicator')).toBe(true);
   });
 
   it('should throw error', () => {
