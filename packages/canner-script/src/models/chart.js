a28ce2b40119d0b236f979a35e2247e7c43be26a
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-22 17:40:26 +0800

    support toolbar in indicator and chart

--- packages/canner-script/src/models/chart.js
+++ packages/canner-script/src/models/chart.js
@@ -5,11 +5,13 @@ import {getRandomKey} from '../layout';
 import type {CannerSchema} from '../flow-types';
 
 export default class ChartModel extends BasicModel {
-  constructor({keyName = getRandomKey() , ...attrs}: CannerSchema) {
+  constructor({keyName = getRandomKey() , ...attrs}: CannerSchema, children: Array<CannerSchema>) {
     super('chart', {
       keyName,
       ...attrs
     });
+
+    this.withToolBar(children);
   }
 
   getDefaultUI() {
