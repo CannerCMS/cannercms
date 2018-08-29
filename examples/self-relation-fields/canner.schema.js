2eac71ebf0b3e2325fb0d3fdf8eb7123b15494a4
Howard Chi <chilijung@gmail.com>
2018-08-29 18:00:56 +0800

    fix indent

--- examples/self-relation-fields/canner.schema.js
+++ examples/self-relation-fields/canner.schema.js
@@ -11,25 +11,25 @@ export default (
       }}
       ui="tree"
     >
-    <string keyName="name" title="Name" />
-    <relation ui="singleSelectTree" keyName="toOneSelfRelation" title="selfRelation" relation={{type: 'toOne', to: 'selfRelation'}}
-      uiParams={{
-        textCol: 'name',
-        columns: [{
-          title: 'Name',
-          dataIndex: 'name'
-        }]
-      }}
-    />
-    <relation ui="multipleSelect" keyName="toManySelfRelation" title="toManySelfRelation" relation={{type: 'toMany', to: 'selfRelation'}}
-      uiParams={{
-        textCol: 'name',
-        columns: [{
-          title: 'Name',
-          dataIndex: 'name'
-        }]
-      }}
-    />
-  </array>
+      <string keyName="name" title="Name" />
+      <relation ui="singleSelectTree" keyName="toOneSelfRelation" title="selfRelation" relation={{type: 'toOne', to: 'selfRelation'}}
+        uiParams={{
+          textCol: 'name',
+          columns: [{
+            title: 'Name',
+            dataIndex: 'name'
+          }]
+        }}
+      />
+      <relation ui="multipleSelect" keyName="toManySelfRelation" title="toManySelfRelation" relation={{type: 'toMany', to: 'selfRelation'}}
+        uiParams={{
+          textCol: 'name',
+          columns: [{
+            title: 'Name',
+            dataIndex: 'name'
+          }]
+        }}
+      />
+    </array>
   </root>
 );
