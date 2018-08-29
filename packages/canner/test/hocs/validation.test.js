739329490d6a69644d27738722f8b4f3b213f57b
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-29 16:26:36 +0800

    Add tests for validation hoc

---
+++ packages/canner/test/hocs/validation.test.js
@@ -0,0 +1,167 @@
+import * as React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Adapter from '../react163Adapter';
+import withValidationn from '../../src/hocs/validation';
+import RefId from 'canner-ref-id';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('withValidation', () => {
+  let WrapperComponent, props, MockComponent, onDeploy, removeOnDeploy;
+
+  beforeEach(() => {
+    MockComponent = function MockComponent() {
+      return (<div>Component</div>);
+    }
+    onDeploy = jest.fn();
+    removeOnDeploy = jest.fn();
+    props = {
+      refId: new RefId('posts/0/url'),
+      onDeploy,
+      removeOnDeploy
+    }
+    WrapperComponent = withValidationn(MockComponent);
+  });
+
+  it('should error state = false', () => {
+    const wrapper = mount(<WrapperComponent
+      {...props}
+    />);
+    expect(wrapper.state()).toEqual({
+      error: false,
+      errorInfo: []
+    })
+  });
+
+  it('should onDeploy be called', () => {
+    const wrapper =  mount(<WrapperComponent
+      {...props}
+      required
+    />);
+    expect(onDeploy).toBeCalledWith('posts', wrapper.instance().validate);
+  });
+
+  it('should pass validation', () => {
+    const result = {
+      data: {
+        0: { url: 'https://'}
+      }
+    };
+    const wrapper =  mount(<WrapperComponent
+      {...props}
+      onDeploy={jest.fn().mockImplementation((_, fn) => (fn(result)))}
+      required
+    />);
+    expect(wrapper.state()).toEqual({
+      error: false,
+      errorInfo: []
+    })
+  });
+
+  it('should not pass required validation', () => {
+    const result = {
+      data: {
+        0: { url: ''}
+      }
+    };
+    const wrapper =  mount(<WrapperComponent
+      {...props}
+      onDeploy={jest.fn().mockImplementation((_, fn) => (fn(result)))}
+      required
+    />);
+    expect(wrapper.state()).toEqual({
+      error: true,
+      errorInfo: [{
+        message: 'should be required'
+      }]
+    })
+  });
+
+  it('should not pass ajv validation', () => {
+    const result = {
+      data: {
+        0: { url: 'imgurl.com'}
+      }
+    };
+    const wrapper =  mount(<WrapperComponent
+      {...props}
+      onDeploy={jest.fn().mockImplementation((_, fn) => (fn(result)))}
+      required
+      validation={{pattern: '^http://[.]+'}}
+    />);
+    expect(wrapper.state()).toMatchObject({
+      error: true,
+      errorInfo: [{
+        message: 'should match pattern "^http://[.]+"'
+      }]
+    })
+  });
+
+  it('should not pass ajv validation with custom error message', () => {
+    const result = {
+      data: {
+        0: { url: 'imgurl.com'}
+      }
+    };
+    const errorMessage = 'custom error';
+    const wrapper =  mount(<WrapperComponent
+      {...props}
+      onDeploy={jest.fn().mockImplementation((_, fn) => (fn(result)))}
+      required
+      validation={{pattern: '^http://[.]+', errorMessage}}
+    />);
+    expect(wrapper.state()).toMatchObject({
+      error: true,
+      errorInfo: [{
+        message: errorMessage
+      }]
+    })
+  });
+
+  it('should use custom validation', () => {
+    const result = {
+      data: {
+        0: { url: ''}
+      }
+    };
+    const wrapper =  mount(<WrapperComponent
+      {...props}
+      onDeploy={jest.fn().mockImplementation((_, fn) => (fn(result)))}
+      validation={
+        {
+          validator: (content, reject) => {
+            if (!content) {
+              return reject('should be required');
+            }
+          }
+        }
+      }
+    />);
+    expect(wrapper.state()).toMatchObject({
+      error: true,
+      errorInfo: [{
+        message: 'should be required'
+      }]
+    })
+  });
+
+  it('should removeOnDeploy not be called', () => {
+    const wrapper = mount(<WrapperComponent
+      {...props}
+    />);
+    wrapper.unmount();
+    expect(removeOnDeploy).not.toBeCalled();
+  });
+
+  it('should removeOnDeploy be called', () => {
+    const wrapper = mount(<WrapperComponent
+      {...props}
+    />);
+    const callbackId = 1;
+    wrapper.instance().callbackId = callbackId;
+    wrapper.unmount();
+    expect(removeOnDeploy).toBeCalledWith('posts', callbackId);
+  });
+});
