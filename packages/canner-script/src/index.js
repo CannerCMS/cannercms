83b5a2b6c9d587661dc1ac19809dcc8c0ad31507
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-31 12:24:13 +0800

    Add actions tag & simplify filter

--- packages/canner-script/src/index.js
+++ packages/canner-script/src/index.js
@@ -16,7 +16,6 @@ import {
   ChartModel,
   IndicatorModel,
   EnumModel,
-  FilterModel,
   PageModel
 } from './models';
 import type {CannerSchema, Props} from './flow-types';
@@ -171,7 +170,17 @@ export default function builder(tag: string | Function, attributes: Object, ...c
       };
     }
     case 'filter': {
-      return new FilterModel(attributes, children).toJson();
+      return {
+        ...attributes,
+        type: 'filter',
+        filters: children
+      };
+    }
+    case 'actions': {
+      return {
+        ...attributes,
+        type: 'actions'
+      };
     }
     case 'Layout': {
       const {visitor, cannerKey} = createLayoutVisitor(attributes, children);
