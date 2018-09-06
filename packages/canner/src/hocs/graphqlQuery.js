8033eb5d61c23345716a09288d2e6923ca6f0ea0
Siou <abz53378@gmail.com>
2018-09-06 16:48:10 +0800

    getValue in graphqlQuery

--- packages/canner/src/hocs/graphqlQuery.js
+++ packages/canner/src/hocs/graphqlQuery.js
@@ -5,15 +5,23 @@ import {Query} from 'react-apollo';
 export default function withQuery(Com) {
   return class ComWithQuery extends React.Component {
     render() {
-      const {graphql, ...restProps} = this.props;
+      const {graphql, getValue, ...restProps} = this.props;
       return (
         <Query query={gql`${graphql}`} >
           {({loading, error, data}) => {
             if (loading) return null;
             if (error) return `Error!: ${error}`;
             const key = Object.keys(data)[0];
+            let value = data[key];
+            if (Array.isArray(value)) {
+              // delete symbol in every item to let vega works
+              value = value.map(v => ({...v}));
+            }
+            if (getValue) {
+              value = getValue(value);
+            }
             return (
-              <Com value={data[key].map(d => ({...d}))} {...restProps} />
+              <Com value={value} {...restProps} />
             );
           }}
         </Query>
