e62e4a6bed14783bf0e83581db66b327ee39aed7
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-09-10 22:42:37 +0800

    Update for relation data

--- packages/canner-helpers/src/createFakeData.js
+++ packages/canner-helpers/src/createFakeData.js
@@ -20,97 +20,132 @@ import type {Schema, SchemaMap} from './types';
 // createFakeData = (schema: Object, listLength: number) => *
 
 
-export default function createFakeData(schema: Schema | SchemaMap, listLength: number = 0) {
-  if (!schema) {
+export default function createFakeData(root: Schema | SchemaMap, listLength: number = 0) {
+  if (!root) {
     return null;
   }
-  if ('type' in schema) {
-    return loop(((schema: any): Schema), listLength);
+  const firstLevelKeys = getFirstLevelKeys(root);
+  return root.hasOwnProperty('type') ? loop(root, root.keyName): mapSchema(root, loop);
+
+  // Loop schema
+  function loop(schema: Schema, key: string) {
+    let result: any;
+
+    switch (schema.type) {
+      case 'object':
+        result = mapSchema(schema.items, loop);
+        result.__typename = result.__typename || null;
+        if (schema.keyName) {
+          result = {
+            [schema.keyName]: result
+          };
+        }
+        break;
+      case 'array':
+        result = [];
+        for (let i = 0; i < listLength; ++i) {
+          let item;
+          if (schema.items && schema.items.items) {
+            item = mapSchema(schema.items.items, loop);
+          } else {
+            item = schema.items.hasOwnProperty('type') ? loop(schema.items, schema.items.keyName): mapSchema(schema.items, loop);
+          }
+          if (typeof item === 'object' && key) {
+            item = {
+              ...item,
+              id: `${key}${i+1}`
+            };
+          } else if (schema.keyName) {
+            item = {
+              [key]: item,
+              id: `${key}${i+1}`
+            }
+          }
+          result.push(item);
+        }
+        break;
+      case 'number':
+        result = faker.random.number();
+        break;
+      case 'geoPoint':
+        result = {
+          __typename: null,
+          lat: faker.address.latitude(),
+          lng: faker.address.longitude(),
+          placeId: faker.random.uuid()
+        };
+        break;
+      case 'dateTime':
+        result = faker.date.past().toISOString();
+        break;
+      case 'file':
+        result = {
+          __typename: null,
+          contentType: faker.system.mimeType(),
+          size: faker.random.number(),
+          name: faker.system.commonFileName(),
+          url: faker.internet.url()
+        };
+        break;
+      case 'image':
+        result = {
+          __typename: null,
+          contentType: 'image/jpeg',
+          size: faker.random.number(),
+          name: faker.system.commonFileName(),
+          url: faker.image.imageUrl()
+        };
+        break;
+      case 'boolean':
+        result = faker.random.boolean();
+        break;
+      case 'string':
+        result = faker.random.word();
+        break;
+      case 'relation': {
+        result = getRelation(schema, firstLevelKeys, listLength);
+        break;
+      }
+      case 'json': {
+        result = {};
+        break;
+      }
+      default:
+        invariant(true, `unsupport type ${schema.type}`);
+        break;
+    }
+    return result;
   }
-  return mapSchema(((schema: any): SchemaMap), (schema: Schema) => loop(schema, listLength));
 }
 
+function getFirstLevelKeys(root: Schema | SchemaMap) {
+  if ('type' in root) {
+    return root.items ? Object.keys(root.items) : {};
+  }
+  return Object.keys(root);
+}
 
-function loop(schema: Schema, listLength: number) {
+function getRelation(schema: Schema, firstLevelKeys: any, listLength: number) {
+  const {type, to} = schema.relation;
+  const isInFirstLevel = Array.isArray(firstLevelKeys) && firstLevelKeys.indexOf(to) > -1 && listLength > 0;
   let result: any;
 
-  switch (schema.type) {
-    case 'object':
-      result = mapSchema(schema.items, (schema: Schema) => loop(schema, listLength));
-      result.__typename = result.__typename || null;
-      if (schema.keyName) {
-        result = {
-          [schema.keyName]: result
-        };
-      }
-      break;
-    case 'array':
-      result = [];
-      for (let i = 0; i < listLength; ++i) {
-        if (schema.items && schema.items.items) {
-          result.push(mapSchema(schema.items.items, (schema: Schema) => loop(schema, listLength)));
-        } else {
-          result.push(loop(schema.items, listLength));
+  switch (type) {
+    case 'toMany':
+      result = {};
+      if (isInFirstLevel) {
+        for(let i = 0; i < listLength; ++i) {
+          result[`${to}${i}`] = Math.random() >= 0.5;
         }
       }
       break;
-    case 'number':
-      result = faker.random.number();
-      break;
-    case 'geoPoint':
-      result = {
-        __typename: null,
-        lat: faker.address.latitude(),
-        lng: faker.address.longitude(),
-        placeId: faker.random.uuid()
-      };
-      break;
-    case 'dateTime':
-      result = faker.date.past().toISOString();
-      break;
-    case 'file':
-      result = {
-        __typename: null,
-        contentType: faker.system.mimeType(),
-        size: faker.random.number(),
-        name: faker.system.commonFileName(),
-        url: faker.internet.url()
-      };
-      break;
-    case 'image':
-      result = {
-        __typename: null,
-        contentType: 'image/jpeg',
-        size: faker.random.number(),
-        name: faker.system.commonFileName(),
-        url: faker.image.imageUrl()
-      };
-      break;
-    case 'boolean':
-      result = faker.random.boolean();
-      break;
-    case 'string':
-      result = faker.random.word();
-      break;
-    case 'relation': {
-      const {type} = schema.relation;
-      switch (type) {
-        case 'toMany':
-          result = [];
-          break;
-        case 'toOne':
-        default:
-          result = null;
-          break;
-      }
-      break;
-    }
-    case 'json': {
-      result = {};
-      break;
-    }
+    case 'toOne':
     default:
-      invariant(true, `unsupport type ${schema.type}`);
+      if (isInFirstLevel) {
+        result = `${to}${Math.floor(Math.random() * listLength) + 1}`;
+      } else {
+        result = null;
+      }
       break;
   }
   return result;
