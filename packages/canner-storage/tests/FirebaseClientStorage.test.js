52e8e7d6ff0b43920c68eef24f5bd9bb0d8444ea
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-09-14 11:38:07 +0800

    Add tests for canner-storage

---
+++ packages/canner-storage/tests/FirebaseClientStorage.test.js
@@ -0,0 +1,28 @@
+import {FirebaseClientStorage} from '../src';
+
+
+describe('FirebaseClientStorage', () => {
+  it('should call getUploadUrl and return link', async () => {
+    const firebase = {
+      snapshot: {
+        downloadURL: 'downloadUrl'
+      }
+    };
+    firebase.storage = jest.fn().mockReturnValue(firebase);
+    firebase.storage.TaskEvent = {STATE_CHANGED: 'STATE_CHANGED'};
+    firebase.ref = jest.fn().mockReturnValue(firebase);
+    firebase.child = jest.fn().mockReturnValue(firebase);
+    firebase.put = jest.fn().mockReturnValue(firebase);
+    firebase.on = jest.fn().mockImplementation((evt, onProgress, reject, resolve) => resolve());
+
+    const arg = {firebase};
+    const storage = new FirebaseClientStorage(arg);
+    const onProgress = jest.fn();
+    const options = {filename: 'filename'};
+    const file = new File(["test"], "test.jpg", {
+      type: "image/jpeg",
+    });
+    const result = await storage.upload(file, options, onProgress);
+    expect(result.link).toBe(firebase.snapshot.downloadURL);
+  });
+});
