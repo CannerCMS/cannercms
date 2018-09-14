c3b83c79c4db7e4d2c10aedd935c653054c6690a
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-09-14 13:30:16 +0800

    Update tests

--- packages/canner-storage/tests/ImgurlStorage.test.js
+++ packages/canner-storage/tests/ImgurlStorage.test.js
@@ -24,11 +24,19 @@ describe('ImgurStorage', () => {
     expect(result.link).toBe('test');
   });
 
-  it('should call axios and return link', async () => {
+  it('should get MASHAPE_API', async () => {
     const arg = {clientId: 'clientId', mashapeKey: 'mashapeKey'};
     const storage = new ImgurStorage(arg);
 
     const result = await storage.getUploadUrl();
-    expect(result.url).toBeDefined();
+    expect(result).toBe('https://imgur-apiv3.p.mashape.com/3/image');
+  });
+
+  it('should get IMGUR_IMAGE_API', async () => {
+    const arg = {clientId: 'clientId'};
+    const storage = new ImgurStorage(arg);
+
+    const result = await storage.getUploadUrl();
+    expect(result).toBe('https://api.imgur.com/3/image/');
   });
 });
