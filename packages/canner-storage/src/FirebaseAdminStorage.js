817f28307b646624a30548dc9a0a5e5773a7874a
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-09-17 15:31:04 +0800

    Update FirebaseAdminStorage.js

--- packages/canner-storage/src/FirebaseAdminStorage.js
+++ packages/canner-storage/src/FirebaseAdminStorage.js
@@ -1,45 +1,13 @@
 // @flow
 
-import axios from 'axios';
-import Promise from 'promise-polyfill';
-import {getUploadPercent} from './utils';
-import type {UploadOptions, OnProgressType} from './types';
+import Storage from './Storage';
+import type {GetUploadUrlType} from './types';
 
-type GetUploadUrlType = (
-  file: File,
-  filePath: string
-) => Promise<{
-  uploadUrl: string,
-  publicUrl: string
-}>;
-
-
-export default class FirebaseAdminStorage {
+export default class FirebaseAdminStorage extends Storage {
   firebase: any;
-  getUploadUrl: GetUploadUrlType;
   
   constructor({firebase, getUploadUrl}: {firebase: any, getUploadUrl: GetUploadUrlType}) {
+    super({getUploadUrl});
     this.firebase = firebase;
-    this.getUploadUrl = getUploadUrl;
-  }
-
-  upload(file: File, options: UploadOptions, onProgress: OnProgressType) {
-    return this.getUploadUrl(file, options.filename)
-      .then(data => {
-        return axios
-          .put(data.url, file, {
-            onUploadProgress: e => (onProgress({ percent: getUploadPercent(e) })),
-            headers: {
-              "Content-Type": file.type,
-              "X-Upload-Content-Type": file.type,
-              "X-Upload-Content-Length": file.size
-            }
-          })
-          .then(() => {
-            return {
-              link: data.url
-            };
-          });
-      });
   }
 }
