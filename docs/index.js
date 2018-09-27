7493c2c1f53014d3662a83ac16d4004275e12fdb
Siou <abz53378@gmail.com>
2018-09-27 15:36:35 +0800

    use remove react-router in demo

--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,9 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import {Menu, notification} from 'antd';
 const SubMenu = Menu.SubMenu;
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Canner from 'packages/canner/src';
 import Container, {transformSchemaToMenuConfig} from 'packages/canner-container/src';
-import R from 'packages/history-router/src';
+import R from 'packages/router/src';
 import schema from './canner.schema';
 import styled from 'styled-components';
 // eslint-disable-next-line
@@ -16,76 +15,80 @@ export const Logo = styled.img`
   width: 200px;
 `;
 
+
+const menuConfig = [
+  {
+    pathname: '__back',
+    title: 'Back to Dashboard',
+    onClick: () => {
+      // eslint-disable-next-line
+      console.log('back to dashboard');
+    },
+    icon: 'left'
+  },
+  ...transformSchemaToMenuConfig({...schema.pageSchema, ...schema.schema})
+]
+
 class CMSExample extends React.Component {
-  constructor(props) {
-    super(props);
+  router = new R({
+    baseUrl: "/docs"
+  })
+
+  componentDidMount() {
+    this.unlisten = this.router.history.listen(() => this.forceUpdate());
+  }
+
+  componentWillUnmount() {
+    this.unlisten();
   }
 
   render() {
-    const baseUrl = "/docs";
-    // eslint-disable-next-line
+    const customHeaderMenu = (
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        style={{ lineHeight: '64px', display: 'inline-block' }}
+        onClick={this.headerMenuOnClick}
+        selectedKeys={[]}
+      >
+        <SubMenu title="Submenu">
+          <Menu.Item theme="light" key="logout">
+            MenuItem 1
+          </Menu.Item>
+        </SubMenu>
+        <Menu.Item>
+         MenuItem 1
+        </Menu.Item>
+      </Menu>
+    )
     return (
-      <Router>
-        <div>
-          <Route path="/docs" render={({history}) => {
-            return (
-              <Container
-                schema={schema}
-                sidebarConfig={{
-                  menuConfig: [
-                    {
-                      pathname: '__back',
-                      title: 'Back to Dashboard',
-                      onClick: () => {
-                        // eslint-disable-next-line
-                        console.log('back to dashboard');
-                      },
-                      icon: 'left'
-                    },
-                    ...transformSchemaToMenuConfig({...schema.pageSchema, ...schema.schema})
-                  ]
-                }}
-                navbarConfig={{
-                  logo: <Logo src="https://cdn.canner.io/cms-page/d89f77c19e5d3aa366ba1498dddd64ef.svg" />,
-                  showSaveButton: true,
-                  renderMenu: () => (<Menu
-                    theme="dark"
-                    mode="horizontal"
-                    style={{ lineHeight: '64px', display: 'inline-block' }}
-                    onClick={this.headerMenuOnClick}
-                    selectedKeys={[]}
-                  ><SubMenu title="submenu">
-                    <Menu.Item theme="light" key="logout">
-                      menuitem
-                    </Menu.Item>
-                  </SubMenu>
-                  <Menu.Item>
-                    menuitem
-                  </Menu.Item></Menu>)
-                }}
-                router={new R({
-                  history,
-                  baseUrl
-                })}
-              >
-                <Canner
-                  afterDeploy={() => {
-                    notification.success({
-                      message: 'Deployed!'
-                    })
-                  }}
-                  errorHandler={e => {
-                    console.error(e);
-                    notification.error({
-                      message: 'Error'
-                    });
-                  }}
-                />
-              </Container>
-            );
-          }}/>
-        </div>
-      </Router>
+      <Container
+        schema={schema}
+        sidebarConfig={{
+          menuConfig
+        }}
+        navbarConfig={{
+          logo: <Logo src="https://cdn.canner.io/cms-page/d89f77c19e5d3aa366ba1498dddd64ef.svg" />,
+          showSaveButton: true,
+          renderMenu: () => customHeaderMenu
+        }}
+        router={this.router}
+      >
+        <Canner
+          afterDeploy={() => {
+            notification.success({
+              message: 'Deployed!'
+            })
+          }}
+          errorHandler={e => {
+            // eslint-disable-next-line no-console
+            console.error(e);
+            notification.error({
+              message: 'Error'
+            });
+          }}
+        />
+      </Container>
     );
   }
 }
@@ -93,3 +96,4 @@ class CMSExample extends React.Component {
 ReactDOM.render(
   <CMSExample />
 , document.getElementById('root'));
+
