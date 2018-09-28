d879da129c49bea9719d5d8aea6d5aa7ef06caf9
Siou <abz53378@gmail.com>
2018-09-28 16:43:43 +0800

    init storage

--- packages/canner-storage/src/CannerFirebaseStorage.js
+++ packages/canner-storage/src/CannerFirebaseStorage.js
@@ -1,9 +1,56 @@
+import axios from 'axios';
+
 // @flow
 
 export default class CannerFirebaseStorage{
-  appId: string;
-  
-  constructor({appId}: {appId: string}) {
+  appId: ?string;
+  apiToken: ?string;
+
+  setToken = (apiToken: string) => {
+    this.apiToken = apiToken;
+    return this;
+  }
+
+  setAppId = (appId: string) => {
     this.appId = appId;
+    return this;
+  }
+
+  getEndpoint() {
+    switch (process.env.NODE_ENV) {
+      case 'development':
+        return '/';
+      case 'production':
+      default:
+        return '/';
+    }
+  }
+
+  getUploadUrl() {
+    const endpoint = this.getEndpoint();
+    return axios.get(endpoint);
+  }
+  
+  upload(file, options, onProgress) {
+    return this.getUploadUrl(file, options.filename)
+      .then(({uploadUrl, publicUrl}) => {
+        return axios
+          .put(uploadUrl, file, {
+            onUploadProgress: e => {
+              const done = e.position || e.loaded;
+              const total = e.totalSize || e.total;
+              const percent = done / total * 100;
+              onProgress({ percent });
+            },
+            headers: {
+              "Content-Type": file.type,
+              "X-Upload-Content-Type": file.type,
+              "X-Upload-Content-Length": file.size
+            }
+          })
+          .then(() => {
+            return {url: publicUrl};
+          })
+      })
   }
 }
