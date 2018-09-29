eb67cb794ae67c1482fad1d41218a3a5551f9bac
chilijung <chilijung@gmail.com>
2018-09-29 16:16:31 +0800

    add catching error message while sidebar menu rendering failes.

---
+++ packages/canner-container/src/components/Error.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Alert } from 'antd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: table;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+  left: 0;
+`;
+
+const Inner = styled.div`
+  display: table-cell;
+  vertical-align: middle;
+`;
+
+const Content = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
+export default class Error extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Inner>
+          <Content>
+            <Alert
+              type='error'
+              showIcon
+              message='Invalid sidebar menu'
+              description={`We occurs error while rendering your sidebar menu's settings, please upload a fixed version`}
+            />
+          </Content>
+        </Inner>
+      </Wrapper>
+    );
+  }
+}
