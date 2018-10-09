64d76a60a1001f48c6c38dbf05de5a248bb9972f
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-10-09 16:51:05 +0800

    Update CannerFirebaseStorage & CannerStorage

--- packages/canner-storage/src/CannerFirebaseStorage.js
+++ packages/canner-storage/src/CannerFirebaseStorage.js
@@ -1,56 +1,18 @@
-import axios from 'axios';
-
 // @flow
+import CannerStorage from './CannerStorage';
 
-export default class CannerFirebaseStorage{
-  appId: ?string;
-  apiToken: ?string;
-
-  setToken = (apiToken: string) => {
-    this.apiToken = apiToken;
-    return this;
+export default class CannerFirebaseStorage extends CannerStorage {
+  constructor() {
+    super();
   }
 
-  setAppId = (appId: string) => {
-    this.appId = appId;
-    return this;
-  }
-
-  getEndpoint() {
+  getEndpoint = () => {
     switch (process.env.NODE_ENV) {
       case 'development':
-        return '/';
+        return '/storage/firebase/uploadUrl';
       case 'production':
       default:
-        return '/';
+        return '/storage/firebase/uploadUrl';
     }
-  }
-
-  getUploadUrl() {
-    const endpoint = this.getEndpoint();
-    return axios.get(endpoint);
-  }
-  
-  upload(file, options, onProgress) {
-    return this.getUploadUrl(file, options.filename)
-      .then(({uploadUrl, publicUrl}) => {
-        return axios
-          .put(uploadUrl, file, {
-            onUploadProgress: e => {
-              const done = e.position || e.loaded;
-              const total = e.totalSize || e.total;
-              const percent = done / total * 100;
-              onProgress({ percent });
-            },
-            headers: {
-              "Content-Type": file.type,
-              "X-Upload-Content-Type": file.type,
-              "X-Upload-Content-Length": file.size
-            }
-          })
-          .then(() => {
-            return {url: publicUrl};
-          })
-      })
   }
 }
