54cfd9f0d58e295d966e62f92ecbb9f8f167512d
Siou <abz53378@gmail.com>
2018-10-31 19:17:42 +0800

    inline layout

--- docs/schema/utils.schema.js
+++ docs/schema/utils.schema.js
@@ -1,2 +1,23 @@
 /** @jsx builder */
-import builder from 'canner-script';
+import builder, {Row, Col} from 'canner-script';
+
+export const Inline = ({children}) => (
+  <Row type="flex" gutter={16}>
+    <Col xs={24} sm={12} md={6} lg={6}>
+      {children[0]}
+    </Col>
+    <Col xs={24} sm={12} md={6} lg={6}>
+      {children[1]}
+    </Col>
+    {children[2] && (
+      <Col xs={24} sm={12} md={6} lg={6}>
+        {children[2]}
+      </Col>
+    )}
+    {children[3] && (
+      <Col xs={24} sm={12} md={6} lg={6}>
+        {children[3]}
+      </Col>
+    )}
+  </Row>
+)
