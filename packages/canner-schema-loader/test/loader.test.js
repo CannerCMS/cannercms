b57713e55c9265715bbe3a04df59e20dec2df4f1
Siou <abz53378@gmail.com>
2018-11-07 17:00:16 +0800

    fix canner-schema-loader test

--- packages/canner-schema-loader/test/loader.test.js
+++ packages/canner-schema-loader/test/loader.test.js
@@ -3,12 +3,11 @@
  */
 
 import compiler from './compiler.js';
-import path from 'path';
 test('Inserts schema and outputs JSONSchema and componentTree', async () => {
-  // FIX: breaks at first install
   const stats = await compiler('test.schema.js', {test: true});
   const output = stats.toJson().modules.find(module => module.name === './test.schema.js').source
-    .replace(new RegExp(path.resolve('../../'), 'g'), 'IGNORED_PATH');
+    // ignore different path for CI
+    .replace(new RegExp(process.cwd(), 'g'), 'IGNORED_PATH');
   expect(output).toMatchSnapshot();
   // const output2 = stats.toJson().modules.find(module => module.name === './schema/string.schema.js').source;
   // expect(output2).toMatchSnapshot();
