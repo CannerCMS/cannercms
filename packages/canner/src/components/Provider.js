67ae3caacd1a5cf648e63039b0161ea26f3cc409
Siou <abz53378@gmail.com>
2018-12-11 20:47:03 +0800

    fix fetchPolicy in create

--- packages/canner/src/components/Provider.js
+++ packages/canner/src/components/Provider.js
@@ -96,7 +96,7 @@ export default class Provider extends React.PureComponent<Props, State> {
     return client.watchQuery({
       query: gql`${gqlStr}`,
       variables,
-      fetchPolicy: routes.length === 1 && operator === 'update' ? 'cache-first' : fetchPolicy
+      fetchPolicy: routes.length > 1 && operator === 'update' ? fetchPolicy : 'cache-first'
     });
   }
 
