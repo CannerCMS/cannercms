b814da1509ddf4bd0cbcaec1664e273142eb8188
Siou <abz53378@gmail.com>
2018-12-26 17:32:56 +0800

    schema for node

---
+++ packages/canner-server/web/client/webpack.common.ts
@@ -0,0 +1,98 @@
+import tsImportPluginFactory from 'ts-import-plugin';
+import path from 'path';
+
+export const ENTRY_PATH = path.join(__dirname, 'index.tsx');
+export const HTML_PATH = path.join(__dirname, 'src/index.html');
+export const WINDOW_VARS_PATH = path.join(__dirname, 'windowVars.ts');
+export const SCHEMA_PATH = path.join(__dirname, 'schema/canner.schema.js');
+export const SCHEMA_ONLY = process.env.SCHENA_ONLY;
+export const WEB_ONLY = process.env.WEB_ONLY;
+
+const plugins = [
+  [require("@babel/plugin-proposal-decorators"), { "legacy": true }],
+  require("@babel/plugin-proposal-function-sent"),
+  require("@babel/plugin-proposal-export-namespace-from"),
+  require("@babel/plugin-proposal-numeric-separator"),
+  require("@babel/plugin-proposal-throw-expressions"),
+  require("@babel/plugin-proposal-export-default-from"),
+  require("@babel/plugin-syntax-dynamic-import"),
+  require("@babel/plugin-syntax-import-meta"),
+  [require("@babel/plugin-proposal-class-properties"), { "loose": false }],
+  require("@babel/plugin-proposal-json-strings"),
+  require("@babel/plugin-transform-modules-commonjs"),
+  [require('babel-plugin-import'), {libraryName: 'antd', style: true}]
+];
+
+export const tsLoader = {
+  oneOf: [{
+    test: /canner\.schema\.tsx?$/,
+    use: [{
+      loader: "canner-schema-loader"
+    }, {
+      loader: 'ts-loader',
+      options: {
+        compilerOptions: {
+          "jsx": "react",
+          "jsxFactory": "CannerScript"
+        }
+      }
+    }]
+  }, {
+    test: /\.tsx?$/,
+    loader: 'ts-loader',
+    options: {
+      transpileOnly: true,
+      getCustomTransformers: () => ({
+        before: [tsImportPluginFactory({
+          libraryName: 'antd',
+          style: true,
+        })]
+      }),
+      compilerOptions: {
+        module: 'es2015'
+      }
+    }
+  }]
+};
+
+export const babelLoader = {
+  oneOf: [{
+    test: /(\.schema\.js|canner\.def\.js)$/,
+    use: [{
+      loader: "canner-schema-loader"
+    }, {
+      loader: "babel-loader",
+      options: {
+        babelrc: false,
+        presets: [
+          "@babel/preset-env",
+          [
+            "@babel/preset-react",
+            {
+              "pragma": "CannerScript", // default pragma is React.createElement
+              "pragmaFrag": "CannerScript.Default", // default is React.Fragment
+              "throwIfNamespace": false // defaults to true
+            }
+          ],
+          "@babel/preset-flow"
+        ],
+        plugins
+      }
+    }]
+  }, {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        babelrc: false,
+        presets: [
+          require("@babel/preset-env"),
+          require("@babel/preset-react"),
+          require("@babel/preset-flow")
+        ],
+        plugins
+      }
+    }
+  }]
+};
--- packages/canner-server/web/client/webpack.config.ts
+++ packages/canner-server/web/client/webpack.config.ts
@@ -1,16 +1,23 @@
 import webpack from 'webpack';
 import HtmlWebPackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import tsImportPluginFactory from 'ts-import-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import path from 'path';
 import CompressionPlugin from 'compression-webpack-plugin';
 import createEntryFile from './src/utils/createEntryFile';
 import createWindowVarsFile from './src/utils/createWindowVarsFile';
+import {
+  tsLoader,
+  babelLoader,
+  ENTRY_PATH,
+  HTML_PATH,
+  SCHEMA_PATH,
+  WINDOW_VARS_PATH,
+  SCHEMA_ONLY,
+  WEB_ONLY
+} from './webpack.common';
 const devMode = process.env.NODE_ENV !== 'production';
-const ENTRY_PATH = path.join(__dirname, 'index.tsx');
-const HTML_PATH = path.join(__dirname, 'src/index.html');
-const WINDOW_VARS_PATH = path.join(__dirname, 'windowVars.ts');
+
 // create entry file dynamic so that we can change appPath, schemaPath by CLI
 createEntryFile({
   entryPath: ENTRY_PATH,
@@ -23,22 +30,38 @@ createWindowVarsFile({
   cloudPath: path.join(__dirname, 'default.canner.cloud.ts')
 });
 
-const plugins = [
-  [require("@babel/plugin-proposal-decorators"), { "legacy": true }],
-  require("@babel/plugin-proposal-function-sent"),
-  require("@babel/plugin-proposal-export-namespace-from"),
-  require("@babel/plugin-proposal-numeric-separator"),
-  require("@babel/plugin-proposal-throw-expressions"),
-  require("@babel/plugin-proposal-export-default-from"),
-  require("@babel/plugin-syntax-dynamic-import"),
-  require("@babel/plugin-syntax-import-meta"),
-  [require("@babel/plugin-proposal-class-properties"), { "loose": false }],
-  require("@babel/plugin-proposal-json-strings"),
-  require("@babel/plugin-transform-modules-commonjs"),
-  [require('babel-plugin-import'), {libraryName: 'antd', style: true}]
-];
 
-const config: webpack.Configuration = {
+const schemaConfig: webpack.Configuration = {
+  target: 'node',
+  entry: SCHEMA_PATH,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'schema.node.js',
+    libraryTarget: 'commonjs'
+  },
+  mode: devMode ? 'development' : 'production',
+  resolve: {
+    "extensions": [".jsx", ".js", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      tsLoader,
+      babelLoader,
+      {
+        test: /\.(le|c)ss$/,
+        loader: 'ignore-loader'
+      }
+    ]
+  },
+  plugins: [
+    // don't generate other chunks
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+};
+
+const webConfig: webpack.Configuration = {
   entry: {
     index: [WINDOW_VARS_PATH, ENTRY_PATH]
   },
@@ -90,78 +113,8 @@ const config: webpack.Configuration = {
   },
   module: {
     rules: [
-      {
-        oneOf: [{
-          test: /canner\.schema\.tsx?$/,
-          use: [{
-            loader: "canner-schema-loader"
-          }, {
-            loader: 'ts-loader',
-            options: {
-              compilerOptions: {
-                "jsx": "react",
-                "jsxFactory": "CannerScript"
-              }
-            }
-          }]
-        }, {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-            getCustomTransformers: () => ({
-              before: [tsImportPluginFactory({
-                libraryName: 'antd',
-                style: true,
-              })]
-            }),
-            compilerOptions: {
-              module: 'es2015'
-            }
-          }
-        }]
-      },
-      {
-        oneOf: [{
-          test: /(\.schema\.js|canner\.def\.js)$/,
-          use: [{
-            loader: "canner-schema-loader"
-          }, {
-            loader: "babel-loader",
-            options: {
-              babelrc: false,
-              presets: [
-                "@babel/preset-env",
-                [
-                  "@babel/preset-react",
-                  {
-                    "pragma": "CannerScript", // default pragma is React.createElement
-                    "pragmaFrag": "CannerScript.Default", // default is React.Fragment
-                    "throwIfNamespace": false // defaults to true
-                  }
-                ],
-                "@babel/preset-flow"
-              ],
-              plugins
-            }
-          }]
-        }, {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              babelrc: false,
-              presets: [
-                require("@babel/preset-env"),
-                require("@babel/preset-react"),
-                require("@babel/preset-flow")
-              ],
-              plugins
-            }
-          }
-        }]
-      },
+      tsLoader,
+      babelLoader,
       {
         test: /\.css$/,
         use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, "css-loader"]
@@ -204,5 +157,15 @@ const config: webpack.Configuration = {
     new CompressionPlugin()
   ]
 };
+
+const config = [];
+
+if (!SCHEMA_ONLY) {
+  config.push(webConfig);
+}
+
+if (!WEB_ONLY) {
+  config.push(schemaConfig);
+}
 
 export default config;
