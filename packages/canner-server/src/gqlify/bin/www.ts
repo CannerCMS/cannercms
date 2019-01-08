9477204a6a89c0094e6618c62238e71341ac07f9
Frank Yang <yangpoan@gmail.com>
2019-01-08 18:16:31 +0800

    add minimist to parse argv

--- packages/canner-server/src/gqlify/bin/www.ts
+++ packages/canner-server/src/gqlify/bin/www.ts
@@ -1,6 +1,12 @@
 import { createApp } from '../app';
+import minimist from 'minimist';
 
-createApp()
+const argv = minimist(process.argv.slice(2));
+if (!argv.schema) {
+  console.log('Please use --schema=<path> give canner.schema.js path')
+}
+
+createApp({ schemaPath: argv.schema })
   .then(({ app }) => {
     return app.listen();
   })
