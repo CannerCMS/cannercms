d5ff49ac4d7341c074aaff80632c5dafad28eb4b
Siou <abz53378@gmail.com>
2019-03-05 12:49:07 +0800

    add test for same name error

--- packages/canner-graphql-interface/test/constants.ts
+++ packages/canner-graphql-interface/test/constants.ts
@@ -45,6 +45,9 @@ export const schema = {
               items: {
                 type: 'string'
               }
+            },
+            author: {
+              type: 'string',
             }
           }
         }
