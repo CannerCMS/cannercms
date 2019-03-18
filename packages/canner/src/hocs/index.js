91e01683a01ae672ce110ae3b2b94803098d5edc
Siou <abz53378@gmail.com>
2019-03-18 19:17:38 +0800

    remove unused hocs

--- packages/canner/src/hocs/index.js
+++ packages/canner/src/hocs/index.js
@@ -1,34 +1,10 @@
-import query from './query';
-import relation from './relation';
-import route from './route';
-import cache from './cache';
-import request from './request';
-import deploy from './deploy';
-import id from './id';
-import title from './title';
-import containerQuery from './containerQuery';
-import onDeploy from './onDeploy';
-import context from './connectContext';
 import errorCatch from './errorCatch';
-import validation from './validation';
 import graphqlQuery from './graphqlQuery';
 import withCanner from './withCanner';
 import withCannerLayout from './withCannerLayout';
 import withCache from './withCache';
 export default {
   errorCatch,
-  query,
-  route,
-  cache,
-  request,
-  deploy,
-  relation,
-  id,
-  title,
-  containerQuery,
-  onDeploy,
-  context,
-  validation,
   graphqlQuery,
   withCanner,
   withCannerLayout,
