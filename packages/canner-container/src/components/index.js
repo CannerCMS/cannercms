25854bd6d3009b9e890365c2aacc238974ea8d4a
Siou <abz53378@gmail.com>
2019-03-21 12:21:57 +0800

    change container to hooks

--- packages/canner-container/src/components/index.js
+++ packages/canner-container/src/components/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import * as React from 'react';
+import React, {useState, useCallback} from 'react';
 import {Layout} from 'antd';
 import {pickBy} from 'lodash';
 import Navbar from './Navbar';
@@ -8,69 +8,42 @@ import Sidebar from './Sidebar';
 // type
 import type {CannerContainerProps, MenuConfig} from './types';
 
-type State = {
-  dataChanged: Object
-};
-
-class CannerContainer extends React.Component<CannerContainerProps, State> {
-  cannerRef: React.ElementRef<any>;
-  menuConfig: MenuConfig | boolean;
-  uiSchema: Object;
-
-  constructor(props: CannerContainerProps) {
-    super(props);
-    const {sidebarConfig, schema} = props;
-    this.uiSchema = {...schema.pageSchema, ...pickBy(schema.schema, v => !v.defOnly)};
-
-    if (sidebarConfig.menuConfig === true) {
-      this.menuConfig = transformSchemaToMenuConfig(this.uiSchema);
-    } else {
-      this.menuConfig = sidebarConfig.menuConfig;
-    }
-
-    this.cannerRef = React.createRef();
-    this.state = {
-      dataChanged: {}
-    }
+function CannerContainer(props: CannerContainerProps) {
+  const {
+    schema,
+    sidebarConfig,
+    navbarConfig,
+    children,
+    router
+  } = props;
+  const uiSchema = {...schema.pageSchema, ...pickBy(schema.schema, v => !v.defOnly)};
+  let menuConfig = sidebarConfig.menuConfig
+  if (sidebarConfig.menuConfig === true) {
+    transformSchemaToMenuConfig(uiSchema);
   }
 
-  dataDidChange = (dataChanged: Object) => {
-    const {dataDidChange} = this.props;
-    if (dataDidChange) {
-      dataDidChange(dataChanged);
+  const cannerRef = React.createRef();
+  const [dataChanged, setDataChanged] = useState({});
+  const routes = router.getRoutes();
+  const routerParams = {
+    operator: router.getOperator(),
+    payload: router.getPayload(),
+    where: router.getWhere(),
+    sort: router.getSort(),
+    pagination: router.getPagination()
+  };
+  const dataDidChange = useCallback((dataChanged: Object) => {
+    if (props.dataDidChange) {
+      props.dataDidChange(dataChanged);
     }
-    this.setState({
-      dataChanged
-    });
-  }
-
-  render() {
-    const {
-      schema,
-      sidebarConfig,
-      navbarConfig,
-      children,
-      router
-    } = this.props;
-
-    const {
-      dataChanged
-    } = this.state;
-
-    const routes = router.getRoutes();
-    const routerParams = {
-      operator: router.getOperator(),
-      payload: router.getPayload(),
-      where: router.getWhere(),
-      sort: router.getSort(),
-      pagination: router.getPagination()
-    };
-    return (
-      <Layout>
+    setDataChanged(dataChanged);
+  }, [])
+  return (
+    <Layout>
         {navbarConfig && (
           <Navbar
             dataChanged={dataChanged}
-            deploy={this.cannerRef.current && this.cannerRef.current.deploy}
+            deploy={cannerRef.current && cannerRef.current.deploy}
             {...navbarConfig}
           />
         )}
@@ -85,17 +58,17 @@ class CannerContainer extends React.Component<CannerContainerProps, State> {
           <Sidebar
             dataChanged={dataChanged}
             goTo={router.goTo}
-            reset={this.cannerRef.current && this.cannerRef.current.reset}
+            reset={cannerRef.current && cannerRef.current.reset}
             routes={routes}
-            schema={this.uiSchema}
+            schema={uiSchema}
             {...sidebarConfig}
-            menuConfig={this.menuConfig}
+            menuConfig={menuConfig}
           />
           <Layout.Content>
             { 
               React.cloneElement(children, {
-                ref: this.cannerRef,
-                dataDidChange: this.dataDidChange,
+                ref: cannerRef,
+                dataDidChange: dataDidChange,
                 schema,
                 goTo: router.goTo,
                 routes,
@@ -105,8 +78,7 @@ class CannerContainer extends React.Component<CannerContainerProps, State> {
           </Layout.Content>
         </Layout>
       </Layout>
-    );
-  }
+  )
 }
 
 export function transformSchemaToMenuConfig(schema: Object): MenuConfig {
