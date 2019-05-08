bbeee3496f142db481624ab9e62d1a6f34e64c00
Siou <abz53378@gmail.com>
2019-05-08 21:09:37 +0800

    remove unused code

--- packages/canner/test/components/provider.test.js
+++ packages/canner/test/components/provider.test.js
@@ -1,20 +1,11 @@
 import * as React from 'react';
-import {render, cleanup, waitForElement} from 'react-testing-library'
+import {render, cleanup} from 'react-testing-library'
 import 'jest-dom/extend-expect';
-import {schema, defaultData, getAuthorById} from './data';
+import {schema, defaultData} from './data';
 import Provider from '../../src/components/Provider';
 import {createClient, MemoryConnector} from 'canner-graphql-interface';
-import {Query} from '../../src/query';
-import gql from "graphql-tag";
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup);
-
-
-function toGQL(schema, key) {
-  const query = new Query({schema});
-  query.toGQL(key);
-  return gql`${query.toGQL(key)}`
-}
 
 describe('provider methods', () => {
   let client;
