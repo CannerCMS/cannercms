cc3953d5eb508c13795f2f5be2684af66abb1eb7
Freda Lai <fredalai00@gmail.com>
2019-05-22 11:49:55 +0800

    Remove useButtons hook and Buttons from item component

--- packages/canner/src/components/item/index.js
+++ packages/canner/src/components/item/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { Row, Col, Alert } from 'antd';
 import { isEmpty, isEqual, isFunction } from 'lodash';
 import { BackButton } from 'canner-helpers';
+import { RENDER_TYPE } from '../../hooks/useRenderType';
 import Label from './Label';
 import ErrorMessage from './ErrorMessage';
-import Buttons from './Buttons';
-import { RENDER_TYPE } from '../../hooks/useRenderType';
 
 export default React.memo((props) => {
   const {
@@ -22,8 +21,6 @@ export default React.memo((props) => {
     errorInfo,
     renderType,
     render,
-    shouldRenderSubmitButton,
-    shouldRenderCancelButton,
   } = props;
   const labelCol = layout === 'horizontal' ? props.labelCol || {
     span: 6,
@@ -60,10 +57,6 @@ export default React.memo((props) => {
         )}
         {renderType === RENDER_TYPE.CHILDREN && renderChildren({ refId })}
         {renderType === RENDER_TYPE.COMPONENT && render(props)}
-        <Buttons
-          shouldRenderCancelButton={shouldRenderCancelButton}
-          shouldRenderSubmitButton={shouldRenderSubmitButton}
-        />
         {
           error && (
             <ErrorMessage>
