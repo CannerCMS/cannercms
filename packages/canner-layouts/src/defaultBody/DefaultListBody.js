498a599e7bb14caeee8d0a1aa36301038f283c7a
Freda Lai <fredalai00@gmail.com>
2019-05-22 18:34:25 +0800

    Add different default Body

---
+++ packages/canner-layouts/src/defaultBody/DefaultListBody.js
@@ -0,0 +1,67 @@
+// @flow
+
+import  React from 'react';
+// $FlowFixMe: antd Breadcrumb
+import {Breadcrumb, Icon} from 'antd';
+import {Item} from 'canner-helpers';
+
+type Props = {
+  id: string,
+  title: string,
+  description: string,
+
+  schema: Object,
+  routes: Array<string>
+};
+
+export default function DefaultListBody({title, description, schema, routes}: Props) {
+  const key = routes[0];
+  const item = schema[key];
+  const breadcrumbs = [{
+    path: 'home',
+    render: () => <Icon type="home" />
+  }, {
+    path: routes[0],
+    render: () => item.title || title
+  }];
+  const itemRender = (route) => {
+    return route.render();
+  };
+  return (
+    <div>
+      <div style={{
+        background: '#fff',
+        borderBottom: '1px solid #eee',
+        padding: '16px 24px'
+      }}>
+        <div style={{
+          marginBottom: 24
+        }}>
+          <Breadcrumb itemRender={itemRender} routes={breadcrumbs} />
+        </div>
+        <h2>{item.title || title}</h2>
+        {
+          (item.description || description) && (
+            <div style={{
+              marginTop: 8
+            }}>
+              {item.description || description}
+            </div>
+          )
+        }
+      </div>
+      <div style={{
+        padding: '16px',
+        background: '#f0f2f5',
+      }}>
+        <div style={{
+          padding: '16px',
+          background: '#fff',
+          minHeight: '100vh'
+        }}>
+          <Item />
+        </div>
+      </div>
+    </div>
+  );
+}
