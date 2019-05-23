b915e0a5b8a88df3aab8f234ded118051cdb8b54
Freda Lai <fredalai00@gmail.com>
2019-05-23 17:24:25 +0800

    Add test for <Body />

--- packages/canner-layouts/test/index.test.js
+++ packages/canner-layouts/test/index.test.js
@@ -1,5 +1,170 @@
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import { cleanup, render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import { Context } from 'canner-helpers';
+import { FORM_TYPE } from 'canner/src/hooks/useFormType';
+import cannerLocales from 'canner/src/components/locale';
+import Body from '../src/Body';
+
+afterEach(cleanup);
+
+const CannerHelperContext = ({ children }) => (
+  <IntlProvider
+    locale="en"
+    messages={{
+      ...cannerLocales.en
+    }}
+  >
+    {children}
+  </IntlProvider>
+);
+
+const keyName = 'posts';
+const renderChildren = () => 'renderChildren';
+const routes = [keyName];
+const schema = {
+  posts: {
+    keyName: 'posts',
+    path: 'posts',
+    title: 'Posts',
+    type: 'array',
+    items: {}
+  }
+};
+
+function CustomizedCreateComponent() {
+  return <div data-testid="customized-create-body">This is component.</div>;
+}
+function CustomizedListComponent() {
+  return <div data-testid="customized-list-body">This is component.</div>;
+}
+function CustomizedUpdateComponent() {
+  return <div data-testid="customized-update-body">This is component.</div>;
+}
+
+
 describe('canner-layouts', () => {
-  it('should have a test', () => {
-    expect(true).toBe(true);
+  describe('if formType is LIST', () => {
+    it('Should render <DefaultListBody />', () => {
+      const { getByTestId } = render(
+        <Context.Provider
+          value={{
+            formType: FORM_TYPE.LIST,
+            renderChildren
+          }}
+        >
+          <Body
+            routes={routes}
+            schema={schema}
+          />
+        </Context.Provider>
+      );
+
+      expect(getByTestId('list-body')).toBeInTheDocument();
+    });
+
+    it('Should render for specifying the listComponent props', () => {
+      const { getByTestId } = render(
+        <Context.Provider
+          value={{
+            formType: FORM_TYPE.LIST,
+            renderChildren
+          }}
+        >
+          <Body
+            routes={[]}
+            listComponent={CustomizedListComponent}
+          />
+        </Context.Provider>
+      );
+
+      expect(getByTestId('customized-list-body')).toBeInTheDocument();
+    });
+  });
+
+  describe('if formType is CREATE', () => {
+    it('Should render <DefaultCreateBody />', () => {
+      const { getByTestId } = render(
+        <CannerHelperContext>
+          <Context.Provider
+            value={{
+              formType: FORM_TYPE.CREATE,
+              renderChildren
+            }}
+          >
+            <Body
+              routes={routes}
+              schema={schema}
+            />
+          </Context.Provider>
+        </CannerHelperContext>
+      );
+
+      expect(getByTestId('create-body')).toBeInTheDocument();
+    });
+
+    it('Should render for specifying the createComponent props', () => {
+      const { getByTestId } = render(
+        <CannerHelperContext>
+          <Context.Provider
+            value={{
+              formType: FORM_TYPE.CREATE,
+              renderChildren
+            }}
+          >
+            <Body
+              routes={[]}
+              createComponent={CustomizedCreateComponent}
+            />
+          </Context.Provider>
+        </CannerHelperContext>
+      );
+
+      expect(getByTestId('customized-create-body')).toBeInTheDocument();
+    });
+  });
+
+  describe('if formType is UPDATE', () => {
+    it('Should render <DefaultUpdateBody />', () => {
+      const { getByTestId } = render(
+        <CannerHelperContext>
+          <Context.Provider
+            value={{
+              formType: FORM_TYPE.UPDATE,
+              renderChildren
+            }}
+          >
+            <Body
+              routes={routes}
+              schema={schema}
+            />
+          </Context.Provider>
+        </CannerHelperContext>
+      );
+
+      expect(getByTestId('update-body')).toBeInTheDocument();
+    });
+
+    it('Should render for specifying the updateComponent props', () => {
+      const { getByTestId } = render(
+        <CannerHelperContext>
+          <Context.Provider
+            value={{
+              formType: FORM_TYPE.UPDATE,
+              renderChildren
+            }}
+          >
+            <Body
+              routes={[]}
+              updateComponent={CustomizedUpdateComponent}
+            />
+          </Context.Provider>
+        </CannerHelperContext>
+      );
+
+      expect(getByTestId('customized-update-body')).toBeInTheDocument();
+    });
   });
 });
