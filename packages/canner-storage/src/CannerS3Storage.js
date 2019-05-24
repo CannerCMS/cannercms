98d002db99ab4ea5325244510e9ea8620695401a
Freda Lai <fredalai00@gmail.com>
2019-05-24 17:33:39 +0800

    Fix no-useless-constructor eslint for CannerS3Storage

--- packages/canner-storage/src/CannerS3Storage.js
+++ packages/canner-storage/src/CannerS3Storage.js
@@ -2,10 +2,6 @@
 import CannerStorage from './CannerStorage';
 
 export default class CannerS3Storage extends CannerStorage {
-  constructor() {
-    super();
-  }
-
   getEndpoint = () => {
     switch (process.env.NODE_ENV) {
       case 'development':
