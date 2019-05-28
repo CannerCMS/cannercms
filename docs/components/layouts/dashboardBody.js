c387cb841e506320be2981da83ed43b6b0b6b621
Siou <abz53378@gmail.com>
2019-05-28 14:49:49 +0800

    function component dashboardBody

--- docs/components/layouts/dashboardBody.js
+++ docs/components/layouts/dashboardBody.js
@@ -1,25 +1,16 @@
 // @flow
 
 import * as React from 'react';
-import {Item} from 'canner-helpers';
+import { Item } from 'canner-helpers';
 
-type Props = {
-  id: string,
-  title: string,
-  description: string,
-  routes: Array<string>
-};
-
-export default class Body extends React.Component<Props> {
-  render() {
-    return (
-      <div style={{
-        padding: '16px',
-        background: '#f0f2f5',
-        minHeight: '100vh'
-      }}>
-        <Item />
-      </div>
-    );
-  }
-}
+export default () => (
+  <div
+    style={{
+      padding: '16px',
+      background: '#f0f2f5',
+      minHeight: '100vh'
+    }}
+  >
+    <Item />
+  </div>
+);
