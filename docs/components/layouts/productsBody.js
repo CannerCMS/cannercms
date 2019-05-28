e0be78b5792e29eedf533d7b499eb5ebe7779763
Siou <abz53378@gmail.com>
2019-05-28 14:48:39 +0800

    productsBody with new buttons API

--- docs/components/layouts/productsBody.js
+++ docs/components/layouts/productsBody.js
@@ -1,8 +1,9 @@
 // @flow
 
 import * as React from 'react';
-import {Breadcrumb, Icon, Card} from 'antd';
-import {Item} from 'canner-helpers';
+import { Breadcrumb, Icon, Card } from 'antd';
+import { Item, BackButton, ConfirmAndCancelButtons } from 'canner-helpers';
+import { FORM_TYPE } from '../../../packages/canner/src/hooks/useFormType';
 
 type Props = {
   id: string,
@@ -13,46 +14,55 @@ type Props = {
   routes: Array<string>
 };
 
-export default class Body extends React.Component<Props> {
-  render() {
-    const {title, description, schema, routes} = this.props;
-    const key = routes[0];
-    const item = schema[key];
-    const breadcrumbs = [{
-      path: 'home',
-      render: () => <Icon type="home" />
-    }, {
-      path: routes[0],
-      render: () => item.title || title
-    }];
-    const itemRender = (route) => {
-      return route.render();
-    }
-    return <div>
-      <div style={{
-        background: '#fff',
-        borderBottom: '1px solid #eee',
-        padding: '16px 24px',
-        position: 'relative'
-      }}>
-        <div style={{
-          marginBottom: 24
-        }}>
+export default (props: Props) => {
+  const {
+    title, description, schema, routes,
+    formType
+  } = props;
+  const key = routes[0];
+  const item = schema[key];
+  const breadcrumbs = [{
+    path: 'home',
+    render: () => <Icon type="home" />
+  }, {
+    path: routes[0],
+    render: () => item.title || title
+  }];
+  const itemRender = route => route.render();
+  return (
+    <div>
+      <div
+        style={{
+          background: '#fff',
+          borderBottom: '1px solid #eee',
+          padding: '16px 24px',
+          position: 'relative'
+        }}
+      >
+        <div
+          style={{
+            marginBottom: 24
+          }}
+        >
           <Breadcrumb itemRender={itemRender} routes={breadcrumbs} />
         </div>
         <h2>{item.title || title}</h2>
         {
           (item.description || description) && (
-            <div style={{
-              marginTop: 8,
-              width: '60%'
-            }}>
+            <div
+              style={{
+                marginTop: 8,
+                width: '60%'
+              }}
+            >
               {item.description || description}
             </div>
           )
         }
         <Links />
-        <img src="https://cdn.canner.io/images/innerPages/banner.png"
+        <img
+          alt="img"
+          src="https://cdn.canner.io/images/innerPages/banner.png"
           height="80%"
           style={{
             position: 'absolute',
@@ -61,24 +71,47 @@ export default class Body extends React.Component<Props> {
           }}
         />
       </div>
-      <div style={{
-        padding: '16px',
-        background: '#f0f2f5',
-        minHeight: '100vh'
-      }}>
+      <div
+        style={{
+          padding: '16px',
+          background: '#f0f2f5',
+          minHeight: '100vh'
+        }}
+      >
+        {
+          (formType === FORM_TYPE.CREATE
+          || formType === FORM_TYPE.UPDATE)
+          && (
+            <BackButton />
+          )
+        }
         <Card>
           <Item />
         </Card>
+        {
+          (formType === FORM_TYPE.CREATE
+          || formType === FORM_TYPE.UPDATE)
+          && (
+            <ConfirmAndCancelButtons
+              shouldRenderSubmitButton
+              shouldRenderCancelButton
+            />
+          )
+        }
       </div>
-    </div>;
-  }
-}
+    </div>
+  );
+};
 
-function Link({icon, name, link}) {
+function Link({ icon, name, link }) {
   return (
-    <a href={link} target="__blank" style={{marginRight: 36}}>
-      <Icon type={icon} theme="outlined"
-        style={{padding: 8, border: '1px solid', borderRadius: '50%', marginRight: 8}}
+    <a href={link} target="__blank" style={{ marginRight: 36 }}>
+      <Icon
+        type={icon}
+        theme="outlined"
+        style={{
+          padding: 8, border: '1px solid', borderRadius: '50%', marginRight: 8
+        }}
       />
       {name}
     </a>
@@ -88,10 +121,20 @@ function Link({icon, name, link}) {
 function Links() {
   return (
     <div
-      style={{marginTop: 24, fontSize: 16, fontWeight: 100}}
+      style={{ marginTop: 24, fontSize: 16, fontWeight: 100 }}
     >
-      <Link icon="file-text" name="Schema" link="https://github.com/Canner/canner/blob/canary/docs/schema/products.schema.js"/>
-      <Link icon="code" name="Customized Body" link="https://github.com/Canner/canner/blob/canary/docs/components/layouts/productsBody.js"/>
+      {/* eslint-disable-next-line */}
+      <Link
+        icon="file-text"
+        name="Schema"
+        link="https://github.com/Canner/canner/blob/canary/docs/schema/products.schema.js"
+      />
+      {/* eslint-disable-next-line */}
+      <Link
+        icon="code"
+        name="Customized Body"
+        link="https://github.com/Canner/canner/blob/canary/docs/components/layouts/productsBody.js"
+      />
     </div>
-  )
+  );
 }
